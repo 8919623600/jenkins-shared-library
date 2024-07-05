9a0cb399804b06e32144ed4bf934dfcc8840acbb
Jyoti sahoo <jyoti29sahoo@gmail.com>
2024-07-05 12:12:59 +0530

    updated

---
+++ vars/python.groovy
@@ -0,0 +1,26 @@
+def lintchecks () {
+          sh "echo performing lint checks for python code"
+          // sh "pip install pylint"
+          sh "echo lint check is completed"
+}
+
+def call () {
+pipeline { 
+    agent any
+    stages {
+        stage('Lint Checks') {
+            steps {
+               script {
+                lintchecks ()
+               }
+
+            }
+        }
+        stage('Static Code Analysis') {
+            steps {
+                sh "echo Static Checks ...."
+            }
+        }
+     }
+  } 
+} 
