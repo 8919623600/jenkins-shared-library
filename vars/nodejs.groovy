d0dc7db20662cfeecfdeca8856cc837820a7d8ca
Jyoti sahoo <jyoti29sahoo@gmail.com>
2024-07-08 16:48:03 +0530

    updated

--- vars/nodejs.groovy
+++ vars/nodejs.groovy
@@ -1,6 +1,6 @@
 def lintchecks () {
      sh "echo Installing Lint Checker for nodejs"
-     sh "npm i jslint"                       // this command will do the lint check for nodejs code
+    //  sh "npm i jslint"                       // this command will do the lint check for nodejs code
      sh "echo This is lint check for $COMPONENT"  // this variable is called from the jenkins pipeline
      sh "node_modules/jslint/bin/jslint.js server.js || true"
 }
