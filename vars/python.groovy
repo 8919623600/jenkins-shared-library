e0faba366e493446940e8447981b83f5553b4e42
Jyoti sahoo <jyoti29sahoo@gmail.com>
2024-07-08 16:40:29 +0530

    updated

--- vars/python.groovy
+++ vars/python.groovy
@@ -1,6 +1,6 @@
 def lintchecks () {
           sh "echo performing lint checks for python code"
-          sh "pip install pylint && pylint *.py"
+          // sh "pip install pylint && pylint *.py"
           sh "echo lint check is completed for $COMPONENT"
 }
 
