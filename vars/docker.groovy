3ddbc2b4121004db6ef08e2eb72d209082a78b73
Jyoti sahoo <jyoti29sahoo@gmail.com>
2024-07-09 16:13:15 +0530

    updated

--- vars/docker.groovy
+++ vars/docker.groovy
@@ -28,7 +28,7 @@ def call() {
         sh "echo Downloading the pen key file for DB Connectivity"
         sh "wget https://truststore.pki.rds.amazonaws.com/global/global-bundle.pem"
         sh "echo Authenticating To ECR"
-        sh "aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 851725330688.dkr.ecr.us-east-1.amazonaws.com"
+        sh "aws ecr-public get-login-password --region us-east-1 | docker login --username AWS --password-stdin public.ecr.aws/s0t0o9e0"
         sh "docker build -t 355449129696.dkr.ecr.us-east-1.amazonaws.com/${COMPONENT}:${TAG_NAME} ."
         sh "docker push 355449129696.dkr.ecr.us-east-1.amazonaws.com/${COMPONENT}:${TAG_NAME}"
         
