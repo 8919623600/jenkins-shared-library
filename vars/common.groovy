d6b0eb72553695cb32205e828af3726adb64901d
Jyoti sahoo <jyoti29sahoo@gmail.com>
2024-07-09 12:31:19 +0530

    updated

--- vars/common.groovy
+++ vars/common.groovy
@@ -1,14 +1,72 @@
-def nodejs_lintchecks () {
-     sh "echo Installing Lint Checker for nodejs"
-    //  sh "npm i jslint"                       // this command will do the lint check for nodejs code
-     sh "echo This is lint check for $COMPONENT"  // this variable is called from the jenkins pipeline
-     sh "node_modules/jslint/bin/jslint.js server.js || true"
+def lintchecks() {
+    stage('Lint Checks') {
+        sh '''
+            echo Installing Lint Checker
+            echo Performing Lint Checks 
+            echo Lint Checks Completed
+        ''' 
+    }
 }
 
-def java_lintchecks () {
-        sh "echo This is a java based project"
-    sh "echo performing lint checks for $COMPONENT"
-    sh "mvn checkstyle:check || true"
-    sh "echo lintcheck is over with the xyz report"
+def testcases() {
+        stage('test cases') {
+        def stages = [:]
+
+        stages["Unit Testing"] = {
+            echo "Unit Testing In Progress"
+            echo "Unit Testing Is Completed"
+        }
+
+        stages["Integration Testing"] = {
+            echo "Integration Testing In Progress"
+            echo "Integration Testing Is Completed"
+        }
+
+        stages["Functional Testing"] = {
+            echo "Functional Testing In Progress"
+            echo "InteFunctionalration Testing Is Completed"
+        }
+
+        parallel(stages)
+    }
 }
 
+def artifacts() {
+    stage('Checkting Artifact On Nexus') {
+        env.upload_status=sh(returnStdout: true, script: "curl -s -L http://172.31.38.109:8081/service/rest/repository/browse/${COMPONENT}/ | grep ${COMPONENT}-${TAG_NAME}.zip || true")
+        print upload_status
+    }
+    
+    if(env.upload_status == "") {
+        stage('Generate Artifacts') {
+            if(env.APPTYPE == "nodejs") {
+                sh "ls -ltr"
+                sh "npm install"
+                sh "zip -r ${COMPONENT}-${TAG_NAME}.zip node_modules/ server.js systemd.service"
+            }
+            else if(env.APPTYPE == "python") {
+                sh "zip -r ${COMPONENT}-${TAG_NAME}.zip *.py *.ini requirements.txt"
+                sh "ls -ltr"
+            }
+            else if(env.APPTYPE == "maven") {
+                sh "mvn clean package"
+                sh "mv target/${COMPONENT}-1.0.jar ${COMPONENT}.jar"
+                sh "zip -r ${COMPONENT}-${TAG_NAME}.zip  ${COMPONENT}.jar systemd.service"
+            }
+            else if(env.APPTYPE == "angularjs") {
+                sh '''cd static/
+                      zip -r ../${COMPONENT}-${TAG_NAME}.zip *
+                      ls -ltr 
+                   ''' 
+
+            }
+        }
+
+        stage('Publish Artifacts') {
+            withCredentials([usernamePassword(credentialsId: 'NEXUS_CRED', passwordVariable: 'NEXUS_PASSWORD', usernameVariable: 'NEXUS_USERNAME')]) {
+                sh "echo Publishing Artifacts"
+                sh "curl -f -v -u ${NEXUS_USERNAME}:${NEXUS_PASSWORD} --upload-file ${COMPONENT}-${TAG_NAME}.zip http://${NEXUS_URL}:8081/repository/${COMPONENT}/${COMPONENT}-${TAG_NAME}.zip"
+            }
+        }
+    }
+}
---
+++ vars/docker.groovy
@@ -0,0 +1,28 @@
+def call() {
+    node {
+        git branch: "main" , url: "https://github.com/8919623600/${COMPONENT}.git"
+        common.lintchecks()
+        common.testcases()
+        if (env.TAG_NAME != null) {
+            stage("generating and publishing artifact")
+            if (env.APPTYPE="nodejs") {
+                sh "echo generating node artifacts"
+                sh "npm install"
+            }
+            else if(env.APPTYPE == "python") {
+                    sh "echo Generating Artifacts"
+                    sh "zip -r ${COMPONENT}-${TAG_NAME}.zip *.py *.ini requirements.txt"        
+                } 
+            else if(env.APPTYPE == "java") {
+                    sh "mvn clean package"
+                    sh "mv target/${COMPONENT}-1.0.jar ${COMPONENT}.jar"
+                    sh "zip -r ${COMPONENT}-${TAG_NAME}.zip  ${COMPONENT}.jar"      
+                }
+            else if(env.APPTYPE == "angularjs") {
+                    sh "cd static/"
+                    sh "zip -r ../${COMPONENT}-${TAG_NAME}.zip *"
+                }
+            else { sh "echo Selected Component Type Doesnt Exist" }                        
+        }
+    }
+}
